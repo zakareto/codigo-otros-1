b3f69719adfafe7f26bc0998cab142c21224e674
zakareto <rafael.alejandro.2000@gmail.com>
2023-06-21 22:13:29 -0600

    ejercicio de revision de codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,23 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('.name');//'name' no tiene el punto que referencia la clase
+const $b = document.querySelector('.blog');//'blog' es una clase asi que debe llevar . no el # que se usa para id
+const $l = document.querySelector('.location');//No existe nada con la clase location
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const response = await fetch(`${usersEndpoint}/${username}`); //Faltaba agregar el modificador async a la funcion
+  const data = await response.json();//Faltaba declarar data
+  console.log(response);//Data no esta definido en ninguna parte
+  $n.textContent = `${data.name}`;//Deben ser backticks ` no '
+  $b.textContent = `${data.blog}`;//Deben ser backticks ` no '
+  $l.textContent = `${data.location}`;//Deben ser backticks ` no '
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`//Le falta el signo de pesos
 }
 
 displayUser('stolinski').catch(handleError);
